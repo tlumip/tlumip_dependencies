b3fc1bc5a86ad8fb56a255163049bbd8dec7fefb
bstabler <benstabler@yahoo.com>
2019-07-05 10:26:15 -0700

    update to visum 18

--- build_swimpy.bat
+++ build_swimpy.bat
@@ -3,4 +3,4 @@ activate swimpy
 conda install cytoolz pytables pandas numpy
 pip install zbox orca openmatrix activitysim ortools pywin32
 pip install https://github.com/RSGInc/populationsim/zipball/master
-XCOPY "C:\Program Files\PTV Vision\PTV Visum 17\Exe\PythonModules\VisumPy" "C:\Program Files\Anaconda2\envs\swim\Lib\site-packages\VisumPy" /i
+XCOPY "C:\Program Files\PTV Vision\PTV Visum 18\Exe\PythonModules\VisumPy" "C:\Program Files\Anaconda2\envs\swim\Lib\site-packages\VisumPy" /i
