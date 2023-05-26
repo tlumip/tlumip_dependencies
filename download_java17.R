798d43653ded1aec2634571b731e10ce2bf31414
Aditya Gore <aditya.gore@rsginc.com>
2023-05-26 14:11:54 -0400

    Added minore updates to R script for cleanup

--- download_java17.R
+++ download_java17.R
@@ -5,14 +5,14 @@ java_url= "https://download.oracle.com/java/17/archive/jdk-17.0.6_windows-x64_bi
 # Download JAVA
 cat(paste0("Downloading \'", java_name, "\' from ", java_url), "\n")
 java_zip_file <- paste0(java_name,".zip")
-#download.file(java_url, java_zip_file)
+download.file(java_url, java_zip_file)
 sevenzip_command <- paste0("7za.exe x ",
                            normalizePath(file.path(getwd(),java_zip_file)),
                            " -o\"", normalizePath(getwd()),"\" -y")
 system(sevenzip_command)
 
 # Remove zip files
-# file.remove(list.files(pattern = ".*\\.zip$", full.names = TRUE))
+file.remove(list.files(pattern = ".*\\.zip$", full.names = TRUE))
 
 # Make Java 17 setup
 if (!dir.exists("java17")) {
